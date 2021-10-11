9d63c031c1c65a567ecf95cbdaaaa088f3b6a87c
HoThiQueLinh <quelinhho@gmail.com>
2021-10-11 21:42:00 +0700

    first commit

---
+++ frm10_11.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace LinhBlack
+{
+    partial class frm10_11
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnbatdau = new System.Windows.Forms.Button();
+            this.btnketthuc = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.timer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thời gian (s)";
+            // 
+            // btnbatdau
+            // 
+            this.btnbatdau.Location = new System.Drawing.Point(35, 151);
+            this.btnbatdau.Name = "btnbatdau";
+            this.btnbatdau.Size = new System.Drawing.Size(75, 23);
+            this.btnbatdau.TabIndex = 1;
+            this.btnbatdau.Text = "Bắt đầu";
+            this.btnbatdau.UseVisualStyleBackColor = true;
+            this.btnbatdau.Click += new System.EventHandler(this.btnbatdau_Click);
+            // 
+            // btnketthuc
+            // 
+            this.btnketthuc.Location = new System.Drawing.Point(169, 151);
+            this.btnketthuc.Name = "btnketthuc";
+            this.btnketthuc.Size = new System.Drawing.Size(75, 23);
+            this.btnketthuc.TabIndex = 2;
+            this.btnketthuc.Text = "Kết thúc";
+            this.btnketthuc.UseVisualStyleBackColor = true;
+            this.btnketthuc.Click += new System.EventHandler(this.btnketthuc_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(295, 135);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "1:30\r\n";
+            // 
+            // timer
+            // 
+            this.timer.Location = new System.Drawing.Point(124, 85);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(120, 22);
+            this.timer.TabIndex = 4;
+            this.timer.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(323, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(465, 372);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frm10_11
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.timer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnketthuc);
+            this.Controls.Add(this.btnbatdau);
+            this.Controls.Add(this.label1);
+            this.Name = "frm10_11";
+            this.Text = "frm10_11";
+            ((System.ComponentModel.ISupportInitialize)(this.timer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnbatdau;
+        private System.Windows.Forms.Button btnketthuc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown timer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ frm10_11.cs
@@ -0,0 +1,67 @@
+﻿using LinhBlack.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+using System.Media;
+
+namespace LinhBlack
+{
+    public partial class frm10_11 : Form
+    {
+        public frm10_11()
+        {
+            InitializeComponent();
+        }
+
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.HappyNewYear);
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (second == 0) { 
+                timer1.Stop();
+                DialogResult dlr=MessageBox.Show("Are you ok (^_^)");
+                if(dlr == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(@"E:\phaohoa.gif ");
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    player.Stream.Position = 0;
+                    player.Play();
+
+                }
+                
+          
+               
+               
+
+            }
+            else
+            {
+                second--;
+                label2.Text = $"{second/60}:{ second%60}";
+            }
+        }
+
+        int second = 0;
+        
+        private void btnbatdau_Click(object sender, EventArgs e)
+        {
+            second = (int)timer.Value *60;
+            timer1.Start();
+        }
+
+        private void btnketthuc_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
